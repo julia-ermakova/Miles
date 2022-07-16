8b29200282783080e77f383a5d88415019356c83
Julia <julia.ermakova94@gmail.com>
2022-07-16 22:16:03 +0700

    initial

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 24876;
+        int per_mile = 20;
+        int miles = amount/per_mile;
+        System.out.println(miles);
+    }
+}
